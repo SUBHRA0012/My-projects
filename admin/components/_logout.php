3cd399d36b6c3600baa0799ffb3498a778747bfd
Subhra Sarkar <113663319+SUBHRA0012@users.noreply.github.com>
2026-02-25 15:38:22 +0530

    Delete admin directory

--- admin/components/_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['admin_loggedin']) && $_SESSION['admin_loggedin']){
-    unset($_SESSION['admin_loggedin']);
-    unset($_SESSION['admin_name']);
-    header('location: ../login.php');
-    exit;
-}
-?>
